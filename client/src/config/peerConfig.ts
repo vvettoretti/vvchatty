a2839430b791174a8d06b0a196552de5ae69fb59
Valerio <valeriovettoretti04@gmail.com>
2022-02-28 10:07:36 +0100

    set STUN servers

--- client/src/config/peerConfig.ts
+++ client/src/config/peerConfig.ts
@@ -4,4 +4,5 @@ export const peerConfig: Peer.PeerJSOption = {
   host: "webrtc.vvettoretti.dev",
   path: "/myapp",
   secure: true,
+  config: { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] },
 };
